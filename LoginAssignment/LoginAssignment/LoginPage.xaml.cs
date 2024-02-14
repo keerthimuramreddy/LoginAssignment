9fe31dbeb7ae42c50d9115c0e5c3050510987dba
kmuramreddy <kmuramreddy@US-GMD7XM3>
2024-02-14 09:42:01 +0530

    Add project files.

---
+++ LoginAssignment/LoginAssignment/LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginAssignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+
+            string username = "admin";
+            string password = "admin";
+
+            
+            string enteredUsername = usernameEntry.Text;
+            string enteredPassword = passwordEntry.Text;
+
+          
+            if (enteredUsername == username && enteredPassword == password)
+            {
+                await DisplayAlert("Success", "Login successful!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "Login failed. Please try again.", "OK");
+            }
+        }
+    }
+}
